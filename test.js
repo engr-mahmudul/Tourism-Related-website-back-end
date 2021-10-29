9b763cad7252273a8a99b479ae744976c8282d9d
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-29 15:31:11 +0600

    chnage testitng

---
+++ test.js
@@ -0,0 +1,24 @@
+const services = [
+    {
+        name: "Sigle Tour Planning", description: "For Single Tour Planning we are well known in Bangladesh. We have been organizing this tour with confidence for a long time.",
+        img: "https://raw.githubusercontent.com/mahmudul-11/Assignment-11-photos/main/1.png", seasion: "Year-round"
+    },
+    {
+        name: "Couple Tour Planning", description: "For Couple Tour Planning we are well known in Bangladesh. We have been organizing this tour with confidence for a long time.",
+        img: "https://raw.githubusercontent.com/mahmudul-11/Assignment-11-photos/main/2.png", seasion: "Year-round"
+    },
+    {
+        name: "Family Tour", description: "For Family and Package Tour we are well known in Bangladesh. We have been organizing this tour with confidence for a long time.",
+        img: "https://raw.githubusercontent.com/mahmudul-11/Assignment-11-photos/main/3.png", seasion: "Year-round"
+    },
+    {
+        name: "Regular Package Tour", description: "For Regular Package Tour we are well known in Bangladesh. We have been organizing this tour with confidence for a long time.",
+        img: "https://raw.githubusercontent.com/mahmudul-11/Assignment-11-photos/main/4.png", seasion: "Year-round"
+    },
+    {
+        name: "International Tour", description: "For International Tour we are well known in Bangladesh. We have been organizing this tour with confidence for a long time.",
+        img: "https://raw.githubusercontent.com/mahmudul-11/Assignment-11-photos/main/5.png", seasion: "Year-round"
+    },
+    { name: "Special Offer Package", description: "For Special Offer Package we are well known in Bangladesh. We have been organizing this tour with confidence for a long time.", img: "https://raw.githubusercontent.com/mahmudul-11/Assignment-11-photos/main/6.png", seasion: "Year-round" }
+
+]
